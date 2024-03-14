756361caaee98aa6976e626a948e3eaf45c35650
mehrdad <93407687+mehrdadmonavari@users.noreply.github.com>
2024-03-14 20:24:05 +0330

    Customize Redix UI theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
-import '@radix-ui/themes/styles.css';
+import "@radix-ui/themes/styles.css";
+import "./theme-config.css";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import NavBar from "./NavBar";
 import { Theme } from "@radix-ui/themes";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 
 export const metadata: Metadata = {
    title: "Create Next App",
@@ -15,10 +16,10 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
    return (
       <html lang="en">
-         <body className={inter.className}>
-            <Theme>
+         <body className={inter.variable}>
+            <Theme appearance="light" accentColor="violet" radius="large">
                <NavBar />
-               <main className='p-5'>{children}</main>
+               <main className="p-5">{children}</main>
             </Theme>
          </body>
       </html>
